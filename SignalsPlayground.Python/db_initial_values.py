898057c037d7d408d487cb56e38253549eaf4a58
Stephen Monaco <stevemonaco@hotmail.com>
2020-06-27 02:35:19 -0400

    Cleanup

--- SignalsPlayground.Python/db_initial_values.py
+++ SignalsPlayground.Python/db_initial_values.py
@@ -4,31 +4,33 @@ import scipy.optimize as optimize
 from wavelet_coefficients import get_scaling_coefficients
 
 # Wavelets from db2 to db25
-wavelet_name = 'db7'
+wavelet_name = 'db4'
 
+# Helper method for minimization: ||Ax||^2
 def f(x):
-    y = np.dot(A, x) - b
+    y = np.dot(A, x)
     return np.dot(y, y)
 
-def map_index(h, i, j):
+# maps 0 or coefficient values to matrix A
+def map_index(b, i, j):
     index = 2 * i - j
-    if index < 0 or index >= len(h):
+    if index < 0 or index >= len(b):
         return 0
     else:
-        return h[index]
+        return b[index]
 
 scaling_coef = np.array(get_scaling_coefficients(wavelet_name))
-h = scaling_coef / (1 / scaling_coef.sum())
+bVector = scaling_coef / (1 / scaling_coef.sum()) # normalize coefficients
 
-N = len(h) // 2
-A_list = [[map_index(h, row, col) for col in range(1, 2*N-1)] for row in range(1, 2*N-1)]
-A = np.array(A_list) - np.identity(2*N - 2)
+N = len(bVector)
+A_list = [[map_index(bVector, row, col) for col in range(1, N-1)] for row in range(1, N-1)]
+A = np.array(A_list) - np.identity(N - 2)
 print(f'A: {A}\n')
 
-b = np.array(np.zeros(2*N - 2))
+zeroVector = np.array(np.zeros(N - 2))
 
 cons = ({'type': 'eq', 'fun': lambda x: x.sum() - 1})
-res = optimize.minimize(f, b, method='SLSQP', constraints=cons, options={'disp' : False})
+res = optimize.minimize(f, zeroVector, method='SLSQP', constraints=cons, options={'disp' : False})
 
 xbest = res['x']
 print(f'{wavelet_name} initial values: {xbest}\n')
